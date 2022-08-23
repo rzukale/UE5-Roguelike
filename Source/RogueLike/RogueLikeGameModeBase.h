d3e1c2c38db4d0c67765800913e1412cbda7609a
rzukale <rzukale@student.hive.fi>
2022-08-23 16:27:09 +0300

    Initial Project commit
    Initialized the project to the correct folder level.

---
+++ Source/RogueLike/RogueLikeGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RogueLikeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROGUELIKE_API ARogueLikeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
